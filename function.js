88a8d575fccf328994a89ce438f38f480126c800
subash281 <tfake2246@gmail.com>
2023-08-02 12:24:14 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,11 @@
+var one=document.getElementById("jk");
+console.log(one);
+one.classList.remove('box');
+// var two=document.getElementById("color");
+// console.log(two);
+one.addEventListener("click",function(){
+    //  one.style.backgroundColor="red";
+//  console.log("hii");
+one.classList.toggle('color')
+});
+
